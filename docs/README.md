7d1508f0869ac2e5d7282d7b4d337996cb3fcfcb
Tomas Hubelbauer <tom.hubelbauer@microsoft.com>
2019-02-21 16:15:54 +0100

    Build and publish the application to GitHub Pages

--- docs/README.md
+++
@@ -1,1 +0,0 @@
-This directory will contain the contents of the `build` directory to be served GitHub Pages.
---
+++ docs/precache-manifest.3d1cb91ca21d3924b136494791097c7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f0ca11691e524b18c944",
+    "url": "/rbc-resizable-event-wrapper/static/js/runtime~main.f0ca1169.js"
+  },
+  {
+    "revision": "753d87ae156e6ed31069",
+    "url": "/rbc-resizable-event-wrapper/static/js/main.753d87ae.chunk.js"
+  },
+  {
+    "revision": "c65f5c55bd3abe5cead7",
+    "url": "/rbc-resizable-event-wrapper/static/js/2.c65f5c55.chunk.js"
+  },
+  {
+    "revision": "753d87ae156e6ed31069",
+    "url": "/rbc-resizable-event-wrapper/static/css/main.62d1e9f8.chunk.css"
+  },
+  {
+    "revision": "c65f5c55bd3abe5cead7",
+    "url": "/rbc-resizable-event-wrapper/static/css/2.e97533fd.chunk.css"
+  },
+  {
+    "revision": "00fc6a1eb3a152c628f6a5fd67d3aa9c",
+    "url": "/rbc-resizable-event-wrapper/index.html"
+  }
+];
